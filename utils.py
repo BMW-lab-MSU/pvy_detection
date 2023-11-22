af033a79d6a1ef32c57fbeae59fbfd9262257bb3
sid <siddat.md.123@gmail.com>
2023-11-21 19:02:34 -0700

    helper functions

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import spectral as sp
 import numpy as np
 
@@ -8,16 +9,25 @@ def read_hyper(header_path):
     rows, cols, bands = hdr.nrows, hdr.ncols, hdr.nbands
     scale_factor = hdr.scale_factor
     wv = hdr.bands.centers
-
-    img = np.zeros((rows, cols, bands))
-    for z in range(bands):
-        img[:, :, z] = hdr.read_band(z) * scale_factor
-
+    img = np.float64(hdr.read_bands(range(0, bands))) * scale_factor
     return img, wv
+
+
+# save extracted hyper data
+def save_extracted_data(file, save_loc):
+    filename = file.split('/')[-1]
+    print(f"Processing file: {filename}")
+    img = read_hyper(file)[0]
+    save_name = os.path.join(save_loc, filename + '.npy')
+    np.save(save_name, img)
+    del img
 
 
 # store the filepaths and other required information
 def info():
     file_directory = {
-        'raw_data': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/raw_radiance_reflectance',
+        'raw_data': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/data/raw_radiance_reflectance',
+        'save_dir': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/data/extracted_data',
     }
+
+    return file_directory
