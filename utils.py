1887050f92f1dc37b52e45765cbd87d84671b2de
sid <siddat.md.123@gmail.com>
2023-11-29 19:03:13 -0700

    add rle decode function

--- utils.py
+++ utils.py
@@ -23,9 +23,29 @@ def save_extracted_data(file, save_loc):
     del img
 
 
+def rle2mask(rle, source_width, source_height, left, top, target_width, target_height):
+    img = np.array(np.zeros([source_height, source_width]))
+    decoded = [0] * (target_width * target_height)  # create bitmap container
+    decoded_idx = 0
+    value = 0
+
+    for v in rle:
+        decoded[decoded_idx:decoded_idx + v] = [value] * v
+        decoded_idx += v
+        value = abs(value - 1)
+
+    decoded = np.array(decoded, dtype=np.uint8)
+    decoded = decoded.reshape((target_height, target_width))  # reshape to image size
+
+    img[top:top + decoded.shape[0], left:left + decoded.shape[1]] = decoded
+
+    return img
+
+
 # store the filepaths and other required information
 def info():
     file_directory = {
+        'general_dir': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/data',
         'raw_data': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/data/raw_radiance_reflectance',
         'save_dir': '/media/SIDSSD/precisionag/Potatoes/MSU Flights/2023-07-12/data/extracted_data',
     }
