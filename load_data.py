ca3a7489c09962d08418587d65a4cd05befe2f65
sid <siddat.md.123@gmail.com>
2023-11-29 19:12:39 -0700

    comments added

--- load_data.py
+++ load_data.py
@@ -10,8 +10,10 @@ def process_file(save_directory, data_kind, data):
     if not os.path.exists(save_loc):
         os.makedirs(save_loc)
 
+    # use partial as there is a fixed value input
     partial_processed = partial(save_extracted_data, save_loc=save_loc)
 
+    # use process instead of threads, else the program will run out of memory
     with ProcessPoolExecutor() as executor:
         executor.map(partial_processed, data)
 
@@ -19,6 +21,7 @@ def process_file(save_directory, data_kind, data):
 def get_data(header_directory, save_directory):
     random.seed(10)
 
+    # 60% for training, 20% for validation and 20% for testing
     random.shuffle(header_directory)
     split_idx_trn = round(len(header_directory) * 0.6)
     split_idx_val = round(len(header_directory) * 0.2)
@@ -40,13 +43,14 @@ def get_data(header_directory, save_directory):
     data_kind = 'test_raw'
     process_file(save_directory, data_kind, test_files)
 
+    # wavelengths should be the same for all the images collected by the same camera
     wvl = read_hyper(train_files[0])[1]
 
     print('---------- Saving other relevant Data ----------')
     save_loc = os.path.join(save_directory, 'other_data')
     if not os.path.exists(save_loc):
         os.makedirs(save_loc)
-    save_loc = os.path.join(save_loc , 'other_data.npz')
+    save_loc = os.path.join(save_loc, 'other_data.npz')  # .npz is used to save multiple variables
     np.savez(save_loc, train_files=train_files, val_files=val_files, test_files=test_files, wvl=wvl)
 
 
@@ -58,4 +62,3 @@ if __name__ == "__main__":
     dir_list = glob.glob(ext_data_folder)
     dir_list.sort()
     get_data(dir_list, save_dir)
-
