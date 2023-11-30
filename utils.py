db371ea256477f032d6517083a760325961c1e49
sid <siddat.md.123@gmail.com>
2023-11-29 19:13:16 -0700

    comments added

--- utils.py
+++ utils.py
@@ -9,16 +9,18 @@ def read_hyper(header_path):
     rows, cols, bands = hdr.nrows, hdr.ncols, hdr.nbands
     scale_factor = hdr.scale_factor
     wv = hdr.bands.centers
+    # float32 is default and goes off-range for last few bands - so convert to float64
     img = np.float64(hdr.read_bands(range(0, bands))) * scale_factor
+    # read_bands reads all the available bands
     return img, wv
 
 
-# save extracted hyper data
+# save extracted hyperspectral data
 def save_extracted_data(file, save_loc):
     filename = file.split('/')[-1]
     print(f"Processing file: {filename}")
     img = read_hyper(file)[0]
-    save_name = os.path.join(save_loc, filename + '.npy')
+    save_name = os.path.join(save_loc, filename + '.npy')   # save as numpy array
     np.save(save_name, img)
     del img
 
