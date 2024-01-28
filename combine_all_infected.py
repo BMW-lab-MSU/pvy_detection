7d9395eb60b6fa66a2e0bb1f20ebddd206d26c58
sid <siddat.md.123@gmail.com>
2024-01-27 17:05:02 -0700

    model training with all positives

--- combine_all_infected.py
+++ combine_all_infected.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import random
 import numpy as np
 import seaborn as sns
@@ -13,117 +14,115 @@ from sklearn.utils import class_weight
 from sklearn.metrics import confusion_matrix
 
 if __name__ == "__main__":
+    all_files = glob.glob(os.path.join(info()['raw_data'], '*.hdr'))
+    all_files.sort()
+    all_first = glob.glob(os.path.join(info()['general_dir'], 'first_derivative', '*.hdr'))
+    all_first.sort()
+    all_second = glob.glob(os.path.join(info()['general_dir'], 'second_derivative', '*.hdr'))
+    all_second.sort()
+    all_labels = glob.glob(os.path.join(info()['save_dir'], 'labels', '*.npy'))
+    all_labels.sort()
+
+    idx_train = [3, 11, 12, 16, 21, 25, 28, 29]
+    idx_val = [6, 20]
+    idx_test = [7, 15]
     idx = list(range(10, 175)) + list(range(185, 244))
-
-    print('Loading First Derivative of Train Raw Data')
-    raw_ddx_1 = os.path.join(info()['general_dir'], 'first_derivative', '2023_06_29_Potatoes_Pika_L_35-Reflectance '
-                                                                        'from Radiance Data and Downwelling '
-                                                                        'Irradiance Spectrum-First Derivative.bip.hdr')
-    img = read_hyper(raw_ddx_1)[0]
-
-    # remove the unwanted bands
-    img = img[:, :, idx]
-
-    print('Loading Second Derivative of Train Raw Data')
-    raw_ddx_1 = os.path.join(info()['general_dir'], 'second_derivative', '2023_06_29_Potatoes_Pika_L_35-Reflectance '
-                                                                         'from Radiance Data and Downwelling '
-                                                                         'Irradiance Spectrum-Second Derivative.bip.hdr')
-    img2 = read_hyper(raw_ddx_1)[0]
-
-    # remove the unwanted bands
-    img2 = img2[:, :, idx]
-
-    img = np.concatenate((img, img2), axis=2)
-
-    print("Loading training data")
-    train_label = np.load(os.path.join(info()['save_dir'], 'labels', '2023_06_29_Potatoes_Pika_L_35-batch-RGB.png.npy'))
-    train_data = np.load(os.path.join(info()['save_dir'], 'train_raw', '2023_06_29_Potatoes_Pika_L_35-Bad Band '
-                                                                       'Removal.bip.hdr.npy'))
-
-    train_data = np.concatenate((train_data, img), axis=2)
-
-    # TEST DATA
-    print('Loading First Derivative of Test Raw Data')
-    raw_ddx_1 = os.path.join(info()['general_dir'], 'first_derivative', '2023_06_29_Potatoes_Pika_L_34-Reflectance '
-                                                                        'from Radiance Data and Downwelling '
-                                                                        'Irradiance Spectrum-First Derivative.bip.hdr')
-    img = read_hyper(raw_ddx_1)[0]
-
-    # remove the unwanted bands
-    img = img[:, :, idx]
-
-    print('Loading Second Derivative of Test Raw Data')
-    raw_ddx_1 = os.path.join(info()['general_dir'], 'second_derivative', '2023_06_29_Potatoes_Pika_L_34-Reflectance '
-                                                                         'from Radiance Data and Downwelling '
-                                                                         'Irradiance Spectrum-Second Derivative.bip.hdr')
-    img2 = read_hyper(raw_ddx_1)[0]
-
-    # remove the unwanted bands
-    img2 = img2[:, :, idx]
-
-    img = np.concatenate((img, img2), axis=2)
-
-    print("Loading test data")
-    test_label = np.load(os.path.join(info()['save_dir'], 'labels', '2023_06_29_Potatoes_Pika_L_34-batch-RGB.png.npy'))
-    test_data = np.load(os.path.join(info()['save_dir'], 'test_raw', '2023_06_29_Potatoes_Pika_L_34-Bad Band '
-                                                                     'Removal.bip.hdr.npy'))
-
-    test_data = np.concatenate((test_data, img), axis=2)
-
-    train_label = train_label.flatten()
-    test_label = test_label.flatten()
-
-    # CHANGE TO WANTED LABELS
     labels_to_keep = [0, 3, 4, 6]
-    # labels_to_keep = [3, 4, 6]
-    # labels_to_keep = [3, 4]
 
-    train_to_keep_idx = np.where(np.isin(train_label, labels_to_keep))[0]
-    test_to_keep_idx = np.where(np.isin(test_label, labels_to_keep))[0]
+    # TRAINING DATA ACCUMULATION
+    train_labels = np.empty((0, len(labels_to_keep)))
+    train_data = np.empty((0, 224 * 3))
+    for i in idx_train:
+        print(f"Processing Train Data, Index number: {i}, filename: {all_files[i]}")
+        img0 = read_hyper(all_files[i])[0]
+        img1 = read_hyper((all_first[i]))[0]
+        img1 = img1[:, :, idx]
+        img2 = read_hyper(all_second[i])[0]
+        img2 = img2[:, :, idx]
+        img = np.concatenate((img0, img1, img2), axis=2)
+        del img0, img1, img2
+        label = np.load(all_labels[i]).flatten()
+        to_keep_idx = np.where(np.isin(label, labels_to_keep))[0]
+        y_val = to_categorical(label[to_keep_idx])
+        y_val = y_val[:, labels_to_keep]
+        img = img.reshape((1800000, 224 * 3))
+        img = img[to_keep_idx, :]
 
-    y_train = to_categorical(train_label[train_to_keep_idx])
-    y_test = to_categorical(test_label[test_to_keep_idx])
+        # keep same amount of 0, 3, and 6 (should be 0, 1, 3) from training data (4 is infected)
+        idx_train_0 = np.where(y_val[:, 0] == 1)[0]
+        idx_train_1 = np.where(y_val[:, 1] == 1)[0]
+        idx_train_2 = np.where(y_val[:, 2] == 1)[0]
+        idx_train_3 = np.where(y_val[:, 3] == 1)[0]
 
-    y_train = y_train[:, labels_to_keep]
-    y_test = y_test[:, labels_to_keep]
+        min_value = np.min([len(idx_train_0), len(idx_train_1), len(idx_train_2), len(idx_train_3)])
 
-    train_data = train_data.reshape((1800000, 224 * 3))
-    test_data = test_data.reshape((1800000, 224 * 3))
+        if min_value > 0:
+            random.seed(10)
+            idx_0 = random.sample(list(idx_train_0), min_value)
+            idx_1 = random.sample(list(idx_train_1), min_value)
+            idx_3 = random.sample(list(idx_train_3), min_value)
 
-    x_train = train_data[train_to_keep_idx, :]
-    x_test = test_data[test_to_keep_idx, :]
+            y_train_idx = np.concatenate([idx_0, idx_1, idx_train_2, idx_3])
+        else:
+            y_train_idx = idx_train_2
 
-    # keep same amount of 0, 3, and 6 (should be 0, 1, 3) from training data (4 is infected)
-    idx_train_0 = np.where(y_train[:, 0] == 1)[0]
-    idx_train_1 = np.where(y_train[:, 1] == 1)[0]
-    idx_train_2 = np.where(y_train[:, 2] == 1)[0]
-    idx_train_3 = np.where(y_train[:, 3] == 1)[0]
+        y_train = y_val[y_train_idx]
+        x_train = img[y_train_idx, :]
+        del img
 
-    random.seed(10)
-    idx_0 = random.sample(list(idx_train_0), round(len(idx_train_0) * 0.3))
-    idx_1 = random.sample(list(idx_train_1), round(len(idx_train_1) * 0.3))
-    # idx_2 = random.sample(list(idx_train_2), round(len(idx_train_2) * 0.3))
-    idx_3 = random.sample(list(idx_train_3), round(len(idx_train_3) * 0.3))
+        train_labels = np.concatenate((train_labels, y_train), axis=0)
+        train_data = np.concatenate((train_data, x_train), axis=0)
 
-    # idx_0 = random.sample(list(idx_train_0), len(idx_train_2) * 2)
-    # idx_1 = random.sample(list(idx_train_1), len(idx_train_2) * 2)
-    # idx_3 = random.sample(list(idx_train_3), len(idx_train_2) * 2)
+    # VALIDATION DATA ACCUMULATION
+    val_labels = np.empty((0, len(labels_to_keep)))
+    val_data = np.empty((0, 224 * 3))
+    for i in idx_val:
+        print(f"Processing Validation Data, Index number: {i}, filename: {all_files[i]}")
+        img0 = read_hyper(all_files[i])[0]
+        img1 = read_hyper((all_first[i]))[0]
+        img1 = img1[:, :, idx]
+        img2 = read_hyper(all_second[i])[0]
+        img2 = img2[:, :, idx]
+        img = np.concatenate((img0, img1, img2), axis=2)
+        del img0, img1, img2
+        label = np.load(all_labels[i]).flatten()
+        to_keep_idx = np.where(np.isin(label, labels_to_keep))[0]
+        y_val = to_categorical(label[to_keep_idx])
+        y_val = y_val[:, labels_to_keep]
+        img = img.reshape((1800000, 224 * 3))
+        img = img[to_keep_idx, :]
 
-    y_train_idx = np.concatenate([idx_0, idx_1, idx_train_2, idx_3])
-    # y_train_idx = np.concatenate([idx_0, idx_train_1, idx_2])
-    # y_train_idx = np.concatenate([idx_0, idx_train_1])
-    y_train = y_train[y_train_idx]
-    x_train = x_train[y_train_idx, :]
+        # keep same amount of 0, 3, and 6 (should be 0, 1, 3) from training data (4 is infected)
+        idx_train_0 = np.where(y_val[:, 0] == 1)[0]
+        idx_train_1 = np.where(y_val[:, 1] == 1)[0]
+        idx_train_2 = np.where(y_val[:, 2] == 1)[0]
+        idx_train_3 = np.where(y_val[:, 3] == 1)[0]
 
-    y_train_flat = np.argmax(y_train, axis=1)
+        min_value = np.min([len(idx_train_0), len(idx_train_1), len(idx_train_2), len(idx_train_3)])
+
+        if min_value > 0:
+            random.seed(10)
+            idx_0 = random.sample(list(idx_train_0), min_value)
+            idx_1 = random.sample(list(idx_train_1), min_value)
+            idx_3 = random.sample(list(idx_train_3), min_value)
+
+            y_train_idx = np.concatenate([idx_0, idx_1, idx_train_2, idx_3])
+        else:
+            y_train_idx = idx_train_2
+
+        y_train = y_val[y_train_idx]
+        x_train = img[y_train_idx, :]
+        del img
+
+        val_labels = np.concatenate((val_labels, y_train), axis=0)
+        val_data = np.concatenate((val_data, x_train), axis=0)
+
+    y_train_flat = np.argmax(train_labels, axis=1)
     class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train_flat), y=y_train_flat)
 
     num_labels = 4
-    # num_labels = 3
-    # num_labels = 2
 
-    # input_size = 224 * 3
-    input_size = (224 * 3, 1)
+    input_size = 224 * 3
     batch_size = 128
     hidden_units = 256
     dropout = 0.5  # Slightly increased dropout for regularization
@@ -131,23 +130,16 @@ if __name__ == "__main__":
     # model is a 3-layer MLP with ReLU and dropout after each layer
     model = Sequential()
 
-    # model.add(Dense(hidden_units, input_dim=input_size))
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=input_size))
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Dropout(dropout))
-    model.add(Dense(hidden_units, kernel_regularizer=l2(0.01)))
-
-    # model.add(Dense(hidden_units, input_dim=input_size, kernel_regularizer=l2(0.01)))
+    model.add(Dense(hidden_units, input_dim=input_size, kernel_regularizer=l2(0.01)))
     model.add(BatchNormalization())  # Add Batch Normalization
-    # model.add(Activation('relu'))
+    model.add(Activation('relu'))
     model.add(LeakyReLU(alpha=0.1))  # Use Leaky ReLU instead of ReLU
     model.add(Dropout(dropout))
 
-    # model.add(Dense(hidden_units))
+    model.add(Dense(hidden_units))
     model.add(Dense(2 * hidden_units, kernel_regularizer=l2(0.01)))
     model.add(BatchNormalization())  # Add Batch Normalization
-    # model.add(Activation('relu'))
+    model.add(Activation('relu'))
     model.add(LeakyReLU(alpha=0.1))
     model.add(Dropout(dropout))
 
@@ -164,16 +156,15 @@ if __name__ == "__main__":
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    # model.compile(loss='binary_crossentropy',
-    #               optimizer='adam',
-    #               metrics=['accuracy'])
-
-    model.fit(x_train,
-              y_train,
-              epochs=20,
+    model.fit(train_data,
+              train_labels,
+              epochs=30,
               batch_size=batch_size,
               shuffle=True,
+              validation_data=(val_data, val_labels),
               class_weight=dict(enumerate(class_weights)))
+
+    np.save(os.path.join(info()['save_dir'], 'model.h5'), model)
 
     # # plot training validation loss and accuracy
     # Access the training history
@@ -183,48 +174,68 @@ if __name__ == "__main__":
     loss = history['loss']
     accuracy = history['accuracy']
     #
-    # # Get validation loss and accuracy if available
-    # val_loss = history.get('val_loss', None)
-    # val_accuracy = history.get('val_accuracy', None)
-    #
-    # # Plotting loss
-    # plt.figure(figsize=(12, 6))
-    # plt.subplot(1, 2, 1)
-    # plt.plot(loss, label='Training Loss')
-    # if val_loss is not None:
-    #     plt.plot(val_loss, label='Validation Loss')
-    # plt.title('Training and Validation Loss')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Loss')
-    # plt.legend()
-    #
-    # # Plotting accuracy
-    # plt.subplot(1, 2, 2)
-    # plt.plot(accuracy, label='Training Accuracy')
-    # if val_accuracy is not None:
-    #     plt.plot(val_accuracy, label='Validation Accuracy')
-    # plt.title('Training and Validation Accuracy')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Accuracy')
-    # plt.legend()
-    #
-    # plt.tight_layout()
+    # Get validation loss and accuracy if available
+    val_loss = history.get('val_loss', None)
+    val_accuracy = history.get('val_accuracy', None)
+
+    # Plotting loss
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(loss, label='Training Loss')
+    if val_loss is not None:
+        plt.plot(val_loss, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # Plotting accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(accuracy, label='Training Accuracy')
+    if val_accuracy is not None:
+        plt.plot(val_accuracy, label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(info()['save_dir'], 'train_val_loss_acc.png'))
     # plt.show()
 
+    # TEST DATA ACCUMULATION
+    test_labels = np.empty((0, len(labels_to_keep)))
+    test_data = np.empty((0, 224 * 3))
+    for i in idx_test:
+        print(f"Processing Test Data, Index number: {i}, filename: {all_files[i]}")
+        img0 = read_hyper(all_files[i])[0]
+        img1 = read_hyper((all_first[i]))[0]
+        img1 = img1[:, :, idx]
+        img2 = read_hyper(all_second[i])[0]
+        img2 = img2[:, :, idx]
+        img = np.concatenate((img0, img1, img2), axis=2)
+        del img0, img1, img2
+        label = np.load(all_labels[i]).flatten()
+        to_keep_idx = np.where(np.isin(label, labels_to_keep))[0]
+        y_val = to_categorical(label[to_keep_idx])
+        y_val = y_val[:, labels_to_keep]
+        img = img.reshape((1800000, 224 * 3))
+        img = img[to_keep_idx, :]
 
-    y_pred = model.predict(x_test)
+        test_labels = np.concatenate((test_labels, y_val), axis=0)
+        test_data = np.concatenate((test_data, img), axis=0)
+
+    predicted = model.predict(test_data)
 
     # Convert predictions and true labels to class labels
-    y_pred_classes = np.argmax(y_pred, axis=1)
-    y_true_classes = np.argmax(y_test, axis=1)
+    predicted_classes = np.argmax(predicted, axis=1)
+    true_classes = np.argmax(test_labels, axis=1)
 
     # Create a confusion matrix
-    conf_matrix = confusion_matrix(y_true_classes, y_pred_classes)
+    conf_matrix = confusion_matrix(true_classes, predicted_classes)
     print(conf_matrix)
 
-    # classes = ['background', 'negative', 'positive', 'resistant negative']
-    # classes = ['negative', 'positive', 'resistant negative']
-    classes = ['negative', 'positive']
+    classes = ['background', 'negative', 'positive', 'resistant negative']
 
     # Plot confusion matrix
     plt.figure(figsize=(8, 6))
@@ -232,4 +243,5 @@ if __name__ == "__main__":
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
     plt.ylabel('True')
-    plt.show()
+    plt.savefig(os.path.join(info()['save_dir'], 'test_conf_mat.png'))
+    # plt.show()
