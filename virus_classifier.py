899c1063ff8fc299d7a06c841beaaf37990d1761
sid <siddat.md.123@gmail.com>
2024-03-07 14:44:06 -0700

    make plots

--- virus_classifier.py
+++ virus_classifier.py
@@ -184,25 +184,128 @@ if __name__ == "__main__":
         _, accuracy = model.evaluate(data_test, labels_test, verbose=2)
         return accuracy  # minimize negative accuracy
 
-    # bounded region of the parameter space
-    pbounds = {
-        'num_units': (16, 512),
-        'num_layers': (1, 6),
-        'learning_rate': (0.0001, 0.1),
-        'batch_size': (4, 8),
-    }
-    optimizer = BayesianOptimization(
-        f=create_model,
-        pbounds=pbounds,
-        random_state=1,
-        verbose=2,
-    )
-    logger = JSONLogger(path=os.path.join(info()['save_dir'], 'virus_classifier_logs.json'))
-    optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-    optimizer.maximize(
-        init_points=5,
-        n_iter=20,
-    )
-    print(optimizer.max)
-    for i, res in enumerate(optimizer.res):
-        print("Iteration {}: \n\t{}".format(i, res))
+    # # bounded region of the parameter space
+    # pbounds = {
+    #     'num_units': (16, 512),
+    #     'num_layers': (1, 6),
+    #     'learning_rate': (0.0001, 0.1),
+    #     'batch_size': (4, 8),
+    # }
+    # optimizer = BayesianOptimization(
+    #     f=create_model,
+    #     pbounds=pbounds,
+    #     random_state=1,
+    #     verbose=2,
+    # )
+    # logger = JSONLogger(path=os.path.join(info()['save_dir'], 'virus_classifier_logs.json'))
+    # optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
+    # optimizer.maximize(
+    #     init_points=5,
+    #     n_iter=20,
+    # )
+    # print(optimizer.max)
+    # for i, res in enumerate(optimizer.res):
+    #     print("Iteration {}: \n\t{}".format(i, res))
+
+    def optimized_model():
+        num_units = 170
+        num_layers = 2
+        batch_size = 2 ** 6
+        learning_rate = 0.072
+
+        model = Sequential()
+        model.add(Dense(num_units, input_dim=input_size, activation='relu'))
+        for _ in range(num_layers - 1):
+            model.add(Dropout(0.2))
+            model.add(Dense(num_units, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        model.add(Dense(1, activation='softmax'))
+        opt = Adam(learning_rate=learning_rate)
+        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+        model.fit(data_train, labels_train, epochs=20, batch_size=batch_size, verbose=2,
+                  validation_data=(data_val, labels_val), shuffle=True, class_weight=class_weights_dict)
+        return model
+
+    # {"target": 0.7714561223983765, "params": {"batch_size": 5.6680880188102964, "learning_rate":
+    # 0.0720604168948716, "num_layers": 1.0005718740867244, "num_units": 165.95695602539251}, "datetime": {
+    # "datetime": "2024-03-05 16:14:47", "elapsed": 0.0, "delta": 0.0}}
+
+    model_save_name = os.path.join(info()['save_dir'], 'model_opt_virus_classifier.keras')
+    if os.path.exists(model_save_name):
+        print('Loading Saved Model')
+        model = keras.models.load_model(model_save_name)
+    else:
+        model = optimized_model()
+        model.save(model_save_name)
+        del data_train, labels_train, data_val, labels_val, data_test, labels_test
+
+    test_data = np.empty((0, 223 * 3))
+    test_labels = np.empty(0)  # there will be 2 labels, 0 and 1
+    idx_test = [15, 13, 1, 18]
+
+    kernel_size = 10
+    kernel1 = np.ones((kernel_size, kernel_size))
+    kernel2 = np.ones((kernel_size, kernel_size, 1))
+    labels_to_keep = [299, 300, 399, 400, 599, 600]  # 3 (healthy), 4 (infected), 6 (resistant) * 10
+
+    for num in idx_test:
+        print(f"Working on Image ", num)
+        img0 = read_hyper(raw[num])[0]
+        img1 = read_hyper(first_der[num])[0]
+        img2 = read_hyper(second_der[num])[0]
+        img = np.concatenate((img0, img1, img2), axis=2)
+        del img0, img1, img2
+        label_whole = np.load(labels[num])
+        down_sampled_label = convolve(label_whole, kernel1, mode='valid')[::kernel_size,
+                             ::kernel_size].flatten().astype(int)
+        down_sampled_img = convolve(img, kernel2, mode='valid')[::kernel_size, ::kernel_size].reshape(
+            (200 * 90, 223 * 3))
+        del img
+        mask = masked_potatoes[:, num].reshape(2000, 900)
+        down_sampled_mask = convolve(mask, kernel1, mode='valid')[::kernel_size, ::kernel_size].flatten().astype(int)
+
+        to_keep_idx = np.where(np.isin(down_sampled_label, labels_to_keep))[0]  # indices with 3, 4, and 6
+        mask_indices = np.where((down_sampled_mask == 99) | (down_sampled_mask == 100))[0]
+
+        overlapped_indices = to_keep_idx[np.where(np.isin(to_keep_idx, mask_indices))[0]]
+        idx_infected = overlapped_indices[
+            np.where((down_sampled_label[overlapped_indices] == 399) | (down_sampled_label[overlapped_indices] == 400))[
+                0]]
+
+        idx_healthy = overlapped_indices[np.where(
+            (down_sampled_label[overlapped_indices] == 299) | (down_sampled_label[overlapped_indices] == 300) | (
+                        down_sampled_label[overlapped_indices] == 599) | (
+                        down_sampled_label[overlapped_indices] == 600))[0]]
+
+        # random.seed(10)
+        # idx_healthy = random.sample(list(idx_healthy), 1000)  # randomly takng 1000 pixels
+        combined_indices = np.hstack((idx_infected, idx_healthy))
+        # random.seed(10)
+        # np.random.shuffle(combined_indices)
+        label_selected = down_sampled_label[combined_indices].astype(int)
+        label_selected[(label_selected == 399) | (label_selected == 400)] = 1
+        label_selected[label_selected != 1] = 0
+        data_selected = down_sampled_img[combined_indices, :]
+        del down_sampled_img
+
+        pred = model.predict(data_selected)
+        threshold = 0.8
+        pred = np.squeeze((pred >= threshold).astype(int))
+
+        img_pred = np.ones(200 * 90) * 2
+        img_pred[combined_indices] = pred
+
+        img_true = np.ones(200 * 90) * 2
+        img_true[combined_indices] = label_selected
+
+        acc = np.sum(np.equal(label_selected, pred)) / len(label_selected)
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(img_true.reshape((200, 90)))
+        plt.subplot(1, 2, 2)
+        plt.imshow(img_pred.reshape((200, 90)))
+        img_num = raw[num].split('_')[-1].split('-')[0]
+        plt.suptitle('Virus True and Pred for Image ' + str(img_num) + '; Accuracy: ' + str(acc))
+        plt_save_name = os.path.join(info()['save_dir'], 'Virus_True_and_Pred_for_Image_' + str(img_num) + '.png')
+        plt.savefig(plt_save_name)
+        plt.close()
